8d80dc189f6dc258dcb38a3dd2c081dc8dc92482
Tehillah Kangamba <>
2021-08-30 00:59:59 -0500

    code style choices

--- src/presenttion/components/SearchBar.js
+++ src/presenttion/components/SearchBar.js
@@ -2,16 +2,10 @@ import '../css/search.css'
 
 
 function SearchBar(props) {
-
-
     return(
-
             <input id={"search-bar"} placeholder={"Search by name"} onChange={
-
                     event => props.update(event.target.value)
-
             } />
-
     )
 }
 export default SearchBar;
